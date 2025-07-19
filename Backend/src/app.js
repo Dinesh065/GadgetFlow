1689877893361d49f7ca1669e1a4656eda48fa72
Dinesh065 <dc9359349132@gmail.com>
2025-07-19 14:31:44 +0530

    Made some minute changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -25,8 +25,7 @@ app.use("/api/v1/webhook", webhookRoutes);
 
 // CORS and other middlewares
 const corsOptions = {
-  origin: "*",
-  // origin: "https://gadget-flow-fjo9.vercel.app",
+  origin: "https://gadget-flow-fjo9.vercel.app",
   credentials: true,
 };
 app.use(cors(corsOptions));
--- Frontend/src/config.jsx
+++ Frontend/src/config.jsx
@@ -1,2 +1,1 @@
-// export const API_BASE_URL = "https://gadget-flow.vercel.app/api/v1";
-export const API_BASE_URL = "http://localhost:8000/api/v1";
+export const API_BASE_URL = "https://gadget-flow.vercel.app/api/v1";
